51900d5ef43884c5f9a5c12cd9ccd98cef9ad8d1
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2023-01-09 23:20:13 +0100

    Add Note.jsx

---
+++ my-app/src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note(){
+    return(
+        <div>
+              <h1 className="Note">This is the note Title</h1>
+               <p>This is the note content</p>
+        </div>
+      
+    );
+}
+
+export default Note;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/Header';
+import Note from './components/Note';
 import Footer from './components/Footer';
 
 
@@ -9,6 +10,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
    <Header/>
+   <Note/>
    <Footer/>
+
   </React.StrictMode>
 );
