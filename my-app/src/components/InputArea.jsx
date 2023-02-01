aa74bb415476b7820904be09833a38cbe43c8862
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2023-02-01 18:55:08 +0100

    Add material-ui Zoom in button

--- my-app/src/components/InputArea.jsx
+++ my-app/src/components/InputArea.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import { Fab } from "@material-ui/core";
+import Zoom from '@material-ui/core/Zoom';
 
 function InputArea(props){
 
@@ -36,11 +37,13 @@ console.log(value);
   }
   
     return(
-        <div>
+        <div> 
          <form>
         <input onChange={handleChange}  placeholder="Title" name="title" value={note.title}></input>
         <textarea onChange={handleChange}  placeholder="Take a note..." name="content" value={note.content} rows="3"></textarea>
-        <Fab onClick={submittedNote}><AddCircleIcon/> </Fab>
+       <Zoom in="true">
+       <Fab onClick={submittedNote}><AddCircleIcon/> </Fab>
+       </Zoom>
        </form>
         </div> 
     );
